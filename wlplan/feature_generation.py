6f1c10d83f7f69af54630b1addc4189eab46d39a
Dillon <dillon.chen1@gmail.com>
2025-03-11 10:35:08 -0400

    isort

--- wlplan/feature_generation.py
+++ wlplan/feature_generation.py
@@ -1,8 +1,15 @@
 import os
 from typing import Optional
 
-from _wlplan.feature_generation import (PruningOptions, _CCWLFeatures, _IWLFeatures, _KWL2Features,
-                                        _LWL2Features, _NIWLFeatures, _WLFeatures)
+from _wlplan.feature_generation import (
+    PruningOptions,
+    _CCWLFeatures,
+    _IWLFeatures,
+    _KWL2Features,
+    _LWL2Features,
+    _NIWLFeatures,
+    _WLFeatures
+)
 from _wlplan.planning import Domain
 
 __all__ = [
